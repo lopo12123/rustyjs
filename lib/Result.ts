58541d8f913e7945003a18e726260abf02299d28
lopo <lopo@zju.edu.cn>
2023-02-01 17:34:55 +0800

    docs: jsdoc

--- lib/Result.ts
+++ lib/Result.ts
@@ -1,48 +1,139 @@
-type RustyResult<Ok = undefined, Err = undefined> = Readonly<{
-    _o?: Ok,
-    _e?: Err,
-    _s: symbol
-    unwrap(): Ok
-}>
+import { JSOption, RustyOption } from "./Option";
 
-class Result {
-    static #ok = Symbol('Result.Ok')
-    static #err = Symbol('Result.Err')
+const _ok = Symbol('JSResult.Ok')
+const _err = Symbol('JSResult.Err')
 
-    // true
-    public static Ok<T = any>(o: T): RustyResult<T, undefined> {
-        const r = {
-            _o: o, _s: Result.#ok,
-            unwrap() {
-                return o
-            }
-        }
-        Object.freeze(r)
-        return r
+class JSResult<Ok = any, Err = any> {
+    readonly #o?: Ok
+    readonly #e?: Err
+    readonly #s: symbol
+
+    constructor(o: Ok, e: Err, ok: boolean) {
+        this.#o = o
+        this.#e = e
+        this.#s = ok ? _ok : _err
     }
 
-    // value
-    public static Err<E = any>(e: E): RustyResult<undefined, E> {
-        const r = {
-            _e: e, _s: Result.#err,
-            unwrap() {
-                throw new TypeError('Result.Err cannot be properly unwrapped.')
-            }
-        }
-        Object.freeze(r)
-        return r
+    /**
+     * @description Returns true if the result is Ok.
+     */
+    public get is_ok(): boolean {
+        return this.#s === _ok
     }
 
-    public static match<T = undefined, E = undefined>(v: RustyResult<T, E>, onOk?: (v?: T) => any, onErr?: (v?: E) => any) {
-        if(v._s === Result.#ok) return onOk?.(v._o)
-        else if(v._s === Result.#err) return onErr?.(v._e)
-        else throw TypeError('The received value is not a valid RustyResult!')
+    /**
+     * @description Returns true if the result is Err.
+     */
+    public get is_err(): boolean {
+        return this.#s === _err
+    }
+
+    /**
+     * @description Returns true if the result is Ok and the value inside of it matches a predicate.
+     */
+    public is_ok_and(predicate: (v: Ok) => boolean): boolean {
+        return this.is_ok && predicate(this.#o!)
+    }
+
+    /**
+     * @description Returns true if the result is Err and the value inside of it matches a predicate.
+     */
+    public is_err_and(predicate: (e: Err) => boolean): boolean {
+        return this.is_err && predicate(this.#e!)
+    }
+
+    /**
+     * @description Converts from JSResult<Ok,Err> to JSOption<Ok>.
+     */
+    public ok(): JSOption<Ok> {
+        return this.is_ok ? RustyOption.Some<Ok>(this.#o!) : RustyOption.None
+    }
+
+    /**
+     * @description Converts from JSResult<Ok,Err> to JSOption<Err>.
+     */
+    public err(): JSOption<Err> {
+        return this.is_err ? RustyOption.Some<Err>(this.#e!) : RustyOption.None
+    }
+
+    /**
+     * @description Returns the contained Ok value. Because this function may panic, its use is generally discouraged.
+     */
+    public unwrap(): Ok {
+        if(this.is_ok) return this.#o!
+
+        throw new TypeError()
+    }
+
+    /**
+     * @description Returns the contained Ok value or a provided defaultValue.
+     */
+    public unwrap_or(defaultValue: Ok): Ok {
+        return this.#o ?? defaultValue
+    }
+
+    /**
+     * @description Returns the contained Ok value or computes it from a closure.
+     */
+    public unwrap_or_else(closure: (e: Err) => Ok): Ok {
+        return this.is_ok ? this.#o! : closure(this.#e!)
+    }
+
+    /**
+     * @description Returns the contained Ok value. Because this function may panic, its use is generally discouraged.
+     */
+    public expect(msg: string): Ok {
+        if(this.is_ok) return this.#o!
+
+        throw new TypeError(msg)
+    }
+
+    /**
+     * @description Returns other if self is Err, otherwise returns the Ok value of self.
+     */
+    public or(other: JSResult<Ok, Err>): JSResult<Ok, Err> {
+        return this.is_ok ? this : other
+    }
+
+    /**
+     * @description Calls op if self is Err, otherwise returns the Ok value of self.
+     */
+    public or_else(op: (e: Err) => JSResult<Ok, Err>): JSResult<Ok, Err> {
+        return this.is_ok ? this : op(this.#e!)
+    }
+
+    /**
+     * @description Returns other if self is Ok, otherwise returns the Err value of self.
+     */
+    public and<Ok2 = any>(other: JSResult<Ok2>): JSResult<Ok2, Err> {
+        // @ts-ignore
+        return this.is_ok ? other : this
+    }
+
+    /**
+     * @description Calls op if the result is Ok, otherwise returns the Err value of self.
+     */
+    public and_then<Ok2 = any>(op: (v: Ok) => JSResult<Ok2>): JSResult<Ok2, Err> {
+        // @ts-ignore
+        return this.is_ok ? op(this.#o) : this
     }
 }
 
-export {
-    Result
+class RustyResult {
+    /**
+     * @description Contains the success value
+     */
+    public static Ok = <Ok = any>(o: Ok): JSResult<Ok, undefined> => new JSResult<Ok, undefined>(o, undefined, true)
+
+    /**
+     * @description Contains the error value
+     */
+    public static Err = <Err = any>(e: Err): JSResult<undefined, Err> => new JSResult<undefined, Err>(undefined, e, false)
+
+    public static match<Ok = any, Err = any, OkResult = any, ErrResult = any>(v: JSResult<Ok, Err>, onOk?: (o?: Ok) => OkResult, onErr?: (e?: Err) => ErrResult): OkResult | ErrResult | undefined {
+        return v.is_ok ? onOk?.(v.ok().unwrap()) : onErr?.(v.err().unwrap())
+    }
 }
-export type {
-    RustyResult
-}
+
+export type { JSResult }
+export { RustyResult }
